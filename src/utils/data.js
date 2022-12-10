ee4fcb3272f2a36ccb3a7600ce8e02d33e38e48b
louie.stokk <louie.stokk@sqli.com>
2022-12-10 14:49:21 +0100

    more reviews

--- src/utils/data.js
+++ src/utils/data.js
@@ -97,5 +97,33 @@ export const customerReviews = [
     kund: "John Pettersson",
     comment:
       "Städningen av vårt dödsbo gick utmärkt. Vi fick ett bra pris för städet och det slängde lite gammla prylar åt oss. Bra firma verkligen."
+  },
+  {
+    id: 7,
+    service: "Sälja dödsbo Göteborg",
+    kund: "Ali Hussein",
+    comment:
+      "Vi ville sälja vårt dödsbo i Göteborg och Dödsbo Jouren hjälpte oss med en helhetslösning där de köpte upp lösöre ur dödsboet, tömde dödsboet, forslade bort dödsboet och sedan slutstädade."
+  },
+  {
+    id: 8,
+    service: "Tömma dödsbo Göteborg",
+    kund: "Lisa Magnusson",
+    comment:
+      "De tömde vårt dödsbo i Göteborg vilket gick utmärkt. Dödsbo Jouren gav oss även en liten slant för våra gammla prylar."
+  },
+  {
+    id: 9,
+    service: "Städning av dödsbo Göteborg",
+    kund: "Britt Marie",
+    comment:
+      "Trevliga killar som gjorde ett jättebra jobb med att städa vårt dödsbo i Göteborg."
+  },
+  {
+    id: 9,
+    service: "Uppköp av dödsbo Göteborg",
+    kund: "Tony Sloth",
+    comment:
+      "Vi fick en kostnadsfri värderin och sedan gjord de ett uppköp av vårt dödsbo i Göteborg."
   }
 ];
