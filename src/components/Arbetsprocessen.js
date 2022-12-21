f883e12c5133d714e66b19287928804c0b1c911a
louie.stokk <louie.stokk@sqli.com>
2022-12-21 14:06:19 +0100

    css upd

--- src/components/Arbetsprocessen.js
+++ src/components/Arbetsprocessen.js
@@ -4,92 +4,78 @@ import { makeStyles } from "@material-ui/core";
 const useStyles = makeStyles({
   root: {
     display: "flex",
-    justifyContent: "space-evenly",
     alignItems: "center",
-    margin: "2rem 0",
-    "@media screen and (max-width: 800px)": {
+    padding: "3rem",
+    "@media screen and (max-width: 1000px)": {
       flexDirection: "column"
     }
   },
   rund: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    border: "1.5px solid black",
     width: "2.6rem",
     height: "2.6rem",
+    margin: "0 auto",
+    border: "1px solid black",
     borderRadius: "50%",
-    "@media screen and (max-width: 800px)": {
-      marginTop: "1rem"
-    }
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
 
 const Arbetsprocessen = () => {
   const classes = useStyles();
   return (
-    <div
-      style={{
-        textAlign: "center",
-        margin: "2rem 0",
-        background: "rgb(248, 247, 247)",
-        height: "650px",
-        padding: "2rem"
-      }}
-      className={classes.container}
-    >
-      <h2 style={{ color: "#0369a1" }}>SÅ FUNGERAR DET</h2>
+    <div>
+      <h2 style={{ color: "#0369a1", textAlign: "center" }}>SÅ FUNGERAR DET</h2>
       <div className={classes.root}>
-        <section
-          style={{
-            width: "33%",
-            alignItems: "center",
-            display: "flex",
-            flexDirection: "column"
-          }}
-        >
+        <section>
           <div className={classes.rund}>
             <p>1</p>
           </div>
-          <p style={{ width: "300px", marginTop: "1rem" }}>
+          <p style={{ width: "300px", marginTop: "0.5rem" }}>
             <strong style={{ color: "#0369a1" }}> VÄRDERING:</strong> Vi
-            erbjuder en kostnadsfri värdering av hemmets föremål för att kunna
-            ge ett fast pris för själva tömningen av dödsboet.
+            erbjuder en kostnadsfri värdering av hemmets föremål, dödsboet eller
+            bohaget. Detta är för att kunna ge er ett fast pris för själva
+            tömningen av dödsboet och så att vi även kan ge er ett pris för
+            uppköpet av lösöret vi är intresserade av att köpa upp.
           </p>
         </section>
-        <section
-          style={{
-            width: "33%",
-            alignItems: "center",
-            display: "flex",
-            flexDirection: "column"
-          }}
-        >
-          <div className={classes.rund}>
+        <section>
+          <div className={classes.rund} style={{ marginTop: "0.5rem" }}>
             <p>2</p>
           </div>
           <p style={{ width: "300px", marginTop: "1rem" }}>
-            <strong style={{ color: "#0369a1" }}> BETALNING: </strong>Är
-            föremålen av högre värde än kostnaderna för arbetet så kommer vi att
-            betala dig som kund för att få tömma dödsboet.
+            <strong style={{ color: "#0369a1" }}>
+              Tömning & Bortforsling:
+            </strong>{" "}
+            Vi kommer den dagen vi bestämt tillsammans för att börja sortera i
+            dödsboet eller bohaget. När vi sorterat klart börjar vi forsla bort
+            det som vi kan skänka eller det som skall gå till tippen. Efter
+            detta tar vi då det lösöre vi köpt upp som vi kör till vår butik
+            eller lager.
           </p>
         </section>
-        <section
-          style={{
-            width: "33%",
-            alignItems: "center",
-            display: "flex",
-            flexDirection: "column"
-          }}
-        >
-          <div className={classes.rund}>
+        <section>
+          <div className={classes.rund} style={{ marginTop: "0.5rem" }}>
             <p>3</p>
           </div>
           <p style={{ width: "300px", marginTop: "1rem" }}>
             <strong style={{ color: "#0369a1" }}> STÄDNING:</strong> Behöver du
-            även hjälp med att få boet städat inför flytten så erbjuder vi även
-            den tjänsten för alla våra dödsbon.
+            även hjälp med att få dödsboet eller bohaget städat så erbjuder vi
+            även den tjänsten för alla våra dödsbon och bohag. Vi gör även
+            saneringar som luktsaneringar av rökande eller annat.
+          </p>
+        </section>
+        <section>
+          <div className={classes.rund}>
+            <p>4</p>
+          </div>
+          <p style={{ width: "300px", marginTop: "1rem" }}>
+            <strong style={{ color: "#0369a1" }}> BETALNING: </strong>Är
+            föremålen/lösöret av högre värde än kostnaderna för arbetet så
+            kommer vi att betala dig som kund för att få tömma dödsboet. Eller
+            så kan det även bli att vi drar av summan mot det ni skall betala
+            oss.
           </p>
         </section>
       </div>
