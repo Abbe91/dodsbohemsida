593bae81baa6904d5557ff77e7e13aa2d0cd7181
louie.stokk <louie.stokk@sqli.com>
2023-03-20 12:15:50 +0100

    seo text footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,16 +9,17 @@ import { useSelector } from "react-redux";
 const Footer = () => {
   const customerData = useSelector(getCustomerData);
   return (
-    <div className="footer" style={{ height: "300px" }}>
-      <div>
-        <h3 style={{ marginBottom: "0.7rem" }}>Dödsbo Jouren</h3>
-        <a href={`tel:${customerData.phone}`}>
-          <AiTwotonePhone style={{ marginBottom: "0.2rem" }} />
-          <p>{customerData.phone}</p>
-        </a>
-      </div>
+    <>
+      <div className="footer" style={{ height: "300px" }}>
+        <div>
+          <h3 style={{ marginBottom: "0.7rem" }}>Dödsbo Jouren</h3>
+          <a href={`tel:${customerData.phone}`}>
+            <AiTwotonePhone style={{ marginBottom: "0.2rem" }} />
+            <p>{customerData.phone}</p>
+          </a>
+        </div>
 
-      {/* <a href={`mailto:${customerData.email}`}>
+        {/* <a href={`mailto:${customerData.email}`}>
         <AiOutlineMail />
         <p>{customerData.email}</p>
       </a>
@@ -29,36 +30,65 @@ const Footer = () => {
       >
         <RiMapPinLine />
       </a> */}
-      <section style={{ margin: "1rem 1rem" }}>
-        <h4 style={{ marginBottom: "0.5rem" }}>Våra kontor/butiker</h4>
-        <div className="office">
-          <div>
-            <h5>Göteborg</h5>
-            <p>Hisingsgatan 10</p>
-            <p>417 03 Göteborg</p>
+        <section style={{ margin: "1rem 1rem" }}>
+          <h4 style={{ marginBottom: "0.5rem" }}>Våra kontor/butiker</h4>
+          <div className="office">
+            <div>
+              <h5>Göteborg</h5>
+              <p>Hisingsgatan 10</p>
+              <p>417 03 Göteborg</p>
+            </div>
+            <div>
+              <h5>Tjörn, Skärhamn</h5>
+              <p>Magasinsgatan 11</p>
+              <p>47 132 Skärhamn</p>
+            </div>
+            <div>
+              <h5>Ulricehamn</h5>
+              <p>Bogesundsgatan 2</p>
+              <p>52 338 Ulricehamn</p>
+            </div>
           </div>
-          <div>
-            <h5>Tjörn, Skärhamn</h5>
-            <p>Magasinsgatan 11</p>
-            <p>47 132 Skärhamn</p>
-          </div>
-          <div>
-            <h5>Ulricehamn</h5>
-            <p>Bogesundsgatan 2</p>
-            <p>52 338 Ulricehamn</p>
-          </div>
-        </div>
+          <p
+            style={{
+              fontSize: "0.7rem",
+              letterSpacing: "1px",
+              margin: "0.2rem 0"
+            }}
+          >
+            Orgnummer: 556469-5194{" "}
+          </p>
+        </section>
+      </div>
+      <div
+        style={{
+          background: "#0369a1",
+          color: "white",
+          height: "300px",
+          textAlign: "center",
+          display: "flex",
+          alignItems: "center"
+        }}
+      >
         <p
           style={{
-            fontSize: "0.7rem",
-            letterSpacing: "1px",
-            margin: "0.2rem 0"
+            maxWidth: "340px",
+            lineHeight: "24px",
+            margin: "0.5rem auto",
+            fontSize: "0.8rem"
           }}
         >
-          Orgnummer: 556469-5194{" "}
+          Välj oss för att få professionell och pålitlig hjälp med att tömma
+          hushåll och dödsbon. Vi har många års erfarenhet av alla typer av
+          flyttar och städningar och vi sanerar dödsbon och hushåll för att ge
+          dig en trygg och ren miljö. Vi erbjuder också värdering av dödsbon och
+          köper upp lösöre för att underlätta processen för dig. Vårt team av
+          experter är alltid redo att hjälpa till med att skapa en smidig och
+          stressfri lösning för alla dina behov. Kontakta oss idag för mer
+          information.
         </p>
-      </section>
-    </div>
+      </div>
+    </>
   );
 };
 
