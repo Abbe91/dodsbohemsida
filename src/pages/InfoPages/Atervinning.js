0647cf03d857529d7bb0355a4de1cdc10ee50146
NoahAkkad <noah.akkad@gmail.com>
2024-06-27 22:12:58 +0200

    Add styling to the atervinning.css page. Remove the old styling from the atervinning.js.

--- src/pages/InfoPages/Atervinning.js
+++ src/pages/InfoPages/Atervinning.js
@@ -70,73 +70,43 @@ const Atervinning = () => {
       <div className="Nav">
         <Nav />
       </div>
-      <div style={{ margin: "1rem 0.5rem", width: "100%" }}>
-        <p style={{ fontSize: "0.9rem" }}>Läs mer om återvinning av</p>
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <div class="base-recycling-center-directory">
+        <p>Läs mer om återvinning av</p>
+        <div class="base-recycling-center-directory-btn">
           {btns.map((btn, i) => {
             return (
-              <a
-                href=""
-                key={i}
-                style={{
-                  fontSize: "0.8rem",
-                  marginRight: "0.5rem",
-                  borderBottom: "1px solid black",
-                }}
-              >
+              <a href="" key={i}>
                 {btn}
               </a>
             );
           })}
         </div>
       </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <h1
-          style={{
-            fontSize: "1.6rem",
-            marginTop: "1rem",
-            marginBottom: "0.5rem",
-            marginLeft: "0.5rem",
-          }}
-        >
-          Återvinning
-        </h1>
-        <section style={{ position: "relative" }}>
+      <div class="base-flex-column-container">
+        <h1 className="base-recycling-center-section-h1">Återvinning</h1>
+        <section class="base-recycling-center-section">
           <img
             src="https://images.pexels.com/photos/3951625/pexels-photo-3951625.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
             alt="återvinning"
             loading="lazy"
-            style={{ width: "100%", height: "200px", objectFit: "cover" }}
           />
-          <article style={{ position: "absolute", top: "20%", left: "0.5rem" }}>
-            <h3
-              style={{
-                background: "white",
-                padding: "0.25rem",
-                maxWidth: "80%",
-              }}
+          <article>
+            <h3>Vi hjälper dig med all typ av återvinning!</h3>
+            <button
+              class="base-recycling-center-section-btn"
+              onClick={handleClick}
             >
-              Vi hjälper dig med all typ av återvinning!
-            </h3>
-            <button className="atv-btn" onClick={handleClick}>
               Gör en gratis förfrågan
             </button>
           </article>
         </section>
-
-        <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
+        <p class="base-recycling-info">
           Här vi har samlat allt du behöver veta om återvinning och
           återvinningstjänster! Här hittar du användbar information och tips om
           hur du kan bidra till en mer hållbar miljö genom att återvinna på rätt
           sätt. Ta del av vår kunskap och gör din del för en bättre framtid!
         </p>
-        <hr></hr>
-        <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
+        <p class="base-recycling-info-description">
           Återvinning är en viktig del av en hållbar framtid. Genom att
           återvinna material istället för att slänga det i soporna kan vi minska
           mängden avfall som hamnar på soptippar och i naturen, vilket i sin tur
@@ -145,9 +115,9 @@ const Atervinning = () => {
           varför det är viktigt, och hur du kan bidra till att minska mängden
           avfall som hamnar på soptipparna.
         </p>
-        <div style={{ marginTop: "2rem" }}>
-          <h3 style={{ marginLeft: "0.5rem" }}>Fakta: återvinning</h3>
-          <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
+        <div class="base-recycling-facts-container">
+          <h3>Fakta: återvinning</h3>
+          <p>
             Återvinning är processen där material som har använts tidigare
             samlas in och omvandlas till nya produkter. Detta inkluderar allt
             från papper och plast till glas och metall. Genom att återvinna
@@ -180,9 +150,9 @@ const Atervinning = () => {
             istället för plast
           </p>
         </div>
-        <div style={{ marginTop: "2rem" }}>
+        <div class="base-recycling-guide-container">
           <h3 style={{ marginLeft: "0.5rem" }}>Hur skall man återvinna?</h3>
-          <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
+          <p>
             <strong>Sortera ditt avfall korrekt</strong>: För att återvinna på
             rätt sätt är det viktigt att sortera ditt avfall korrekt. Detta
             innebär att du bör separera olika typer av material och lägga dem i
@@ -213,11 +183,9 @@ const Atervinning = () => {
             renare miljö.
           </p>
         </div>
-        <div style={{ marginTop: "2rem" }}>
-          <h3 style={{ marginLeft: "0.5rem" }}>
-            Vad är viktigt att tänka på när man återvinner?
-          </h3>
-          <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
+        <div class="base-recycling-tips-container">
+          <h3>Vad är viktigt att tänka på när man återvinner?</h3>
+          <p>
             <strong>Rengör och sortera rätt</strong>: För att återvinna på rätt
             sätt är det viktigt att sortera ditt avfall korrekt. Detta innebär
             att du bör separera olika typer av material och lägga dem i lämpliga
@@ -249,100 +217,106 @@ const Atervinning = () => {
             sätt och bidra till en mer hållbar framtid.
           </p>
         </div>
-        <div style={{ marginTop: "2rem" }}>
-          <h3 style={{ marginLeft: "0.5rem" }}>
+        <div class="base-recycling-best-practices">
+          <h3>
             GUIDE: såhär återvinner du på bästa sättet för att värna om miljön
           </h3>
-          <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
+          <p>
             Att återvinna på bästa sättet är en viktig del av att ta hand om vår
             planet och minska vår miljöpåverkan. Här är några tips för att
             återvinna på bästa sätt och värna om miljön:
           </p>
-          <section>
-            <h4 style={{ margin: "0.2rem 0.5rem" }}>
-              1. Sortera ditt avfall korrekt
-            </h4>
-            <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
-              För att återvinna på rätt sätt är det viktigt att sortera ditt
-              avfall korrekt. Detta innebär att du bör separera olika typer av
-              material och lägga dem i lämpliga återvinningsbehållare. Det
-              vanligaste materialet som återvinns är papper, plast, glas och
-              metall. Se till att du har dessa behållare tillgängliga på rätt
-              plats.
-            </p>
-          </section>
-          <section>
-            <h4 style={{ margin: "0.2rem 0.5rem" }}>
-              2. Rengör och sortera rätt
-            </h4>
-            <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
-              För att säkerställa att ditt återvinningsmaterial kan användas på
-              nytt är det viktigt att det är rent och utan restprodukter. Se
-              till att du har rengjort dina återvinningsmaterial ordentligt och
-              sortera dem enligt de olika materialen. Om du är osäker på hur du
-              ska sortera ditt avfall, kontakta din kommun eller
-              återvinningscentral för information.
-            </p>
-          </section>
-          <section>
-            <h4 style={{ margin: "0.2rem 0.5rem" }}>3. Återanvänd produkter</h4>
-            <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
-              Att återanvända produkter är en annan effektiv metod för att
-              minska avfallet och spara resurser. Du kan använda återanvändbara
-              produkter som tygkassar, glasflaskor, matlådor, bestick och
-              tallrikar istället för att använda engångsprodukter. Detta kan
-              också leda till en minskning av dina utgifter för
-              hushållsartiklar.
-            </p>
-          </section>
-          <section>
-            <h4 style={{ margin: "0.2rem 0.5rem" }}>4. Sälja eller donera</h4>
-            <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
-              Fundera på att sälja eller donera saker som fortfarande fungerar
-              och kan ha ett andra liv istället för att kasta dem i soporna. Det
-              kan vara möjligt att sälja eller donera elektronik, kläder och
-              möbler till organisationer eller second-hand-butiker. På så sätt
-              kan du inte bara minska avfallsmängderna utan också bidra till att
-              skapa en mer hållbar och cirkulär ekonomi.
-            </p>
-          </section>
-          <section>
-            <h4 style={{ margin: "0.2rem 0.5rem" }}>
-              5. Undvik engångsprodukter
-            </h4>
-            <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
-              Undvik att använda engångsprodukter som plastpåsar, engångsmuggar,
-              engångsbestick och engångstallrikar. De är ofta svåra att
-              återvinna och kan orsaka skada på miljön. Använd istället
-              återanvändbara alternativ.
-            </p>
-          </section>
-          <section>
-            <h4 style={{ margin: "0.2rem 0.5rem" }}>
-              6. Kontrollera lokala återvinningsprogram
-            </h4>
-            <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
-              Kolla med din kommun eller återvinningscentral vad som återvinns
-              på din ort och var det finns insamlingsstationer. Det kan också
-              vara möjligt att ha hämtning av återvinningsmaterial från ditt
-              hem. Genom att utnyttja de återvinningsmöjligheter som finns
-              tillgängliga i din kommun, kan du bidra till en mer hållbar
-              framtid.
-            </p>
-          </section>
-          <section>
-            <h4 style={{ margin: "0.2rem 0.5rem" }}>6. Välj återvunnet</h4>
-            <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
-              När du handlar, välj produkter som innehåller återvunnet material
-              istället för nyproducerat material
-            </p>
-          </section>
+          <ol>
+            <section>
+              <h4>
+                <li>Sortera ditt avfall korrekt</li>
+              </h4>
+              <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
+                För att återvinna på rätt sätt är det viktigt att sortera ditt
+                avfall korrekt. Detta innebär att du bör separera olika typer av
+                material och lägga dem i lämpliga återvinningsbehållare. Det
+                vanligaste materialet som återvinns är papper, plast, glas och
+                metall. Se till att du har dessa behållare tillgängliga på rätt
+                plats.
+              </p>
+            </section>
+            <section>
+              <h4>
+                <li>Rengör och sortera rätt</li>
+              </h4>
+              <p>
+                För att säkerställa att ditt återvinningsmaterial kan användas
+                på nytt är det viktigt att det är rent och utan restprodukter.
+                Se till att du har rengjort dina återvinningsmaterial ordentligt
+                och sortera dem enligt de olika materialen. Om du är osäker på
+                hur du ska sortera ditt avfall, kontakta din kommun eller
+                återvinningscentral för information.
+              </p>
+            </section>
+            <section>
+              <h4>
+                <li>Återanvänd produkter</li>
+              </h4>
+              <p>
+                Att återanvända produkter är en annan effektiv metod för att
+                minska avfallet och spara resurser. Du kan använda
+                återanvändbara produkter som tygkassar, glasflaskor, matlådor,
+                bestick och tallrikar istället för att använda engångsprodukter.
+                Detta kan också leda till en minskning av dina utgifter för
+                hushållsartiklar.
+              </p>
+            </section>
+            <section>
+              <h4>
+                <li>Sälja eller donera</li>
+              </h4>
+              <p>
+                Fundera på att sälja eller donera saker som fortfarande fungerar
+                och kan ha ett andra liv istället för att kasta dem i soporna.
+                Det kan vara möjligt att sälja eller donera elektronik, kläder
+                och möbler till organisationer eller second-hand-butiker. På så
+                sätt kan du inte bara minska avfallsmängderna utan också bidra
+                till att skapa en mer hållbar och cirkulär ekonomi.
+              </p>
+            </section>
+            <section>
+              <h4>
+                <li>Undvik engångsprodukter</li>
+              </h4>
+              <p>
+                Undvik att använda engångsprodukter som plastpåsar,
+                engångsmuggar, engångsbestick och engångstallrikar. De är ofta
+                svåra att återvinna och kan orsaka skada på miljön. Använd
+                istället återanvändbara alternativ.
+              </p>
+            </section>
+            <section>
+              <h4>
+                <li>Kontrollera lokala återvinningsprogram</li>
+              </h4>
+              <p>
+                Kolla med din kommun eller återvinningscentral vad som återvinns
+                på din ort och var det finns insamlingsstationer. Det kan också
+                vara möjligt att ha hämtning av återvinningsmaterial från ditt
+                hem. Genom att utnyttja de återvinningsmöjligheter som finns
+                tillgängliga i din kommun, kan du bidra till en mer hållbar
+                framtid.
+              </p>
+            </section>
+            <section>
+              <h4>
+                <li>Välj återvunnet</li>
+              </h4>
+              <p>
+                När du handlar, välj produkter som innehåller återvunnet
+                material istället för nyproducerat material
+              </p>
+            </section>
+          </ol>
         </div>
-        <div style={{ marginTop: "2rem" }}>
-          <h3 style={{ marginLeft: "0.5rem" }}>
-            Hur kan vi hjälpa dig med återvinning?
-          </h3>
-          <p style={{ fontSize: "0.85rem", margin: "0.2rem 0.5rem" }}>
+        <div class="base-recycling-services-description">
+          <h3>Hur kan vi hjälpa dig med återvinning?</h3>
+          <p>
             Vi kan hjälpa dig med all typ av återvinning, oavsett om det handlar
             om att återvinna papper, plast, metall eller annat avfall. Vår
             målsättning är att hjälpa våra kunder att minska sin miljöpåverkan
@@ -361,10 +335,12 @@ const Atervinning = () => {
             er och göra en positiv skillnad för miljön.
           </p>
         </div>
-        <button className="atv-btn" onClick={handleClick}>
+        <button
+          class="base-recycling-services-description-btn"
+          onClick={handleClick}
+        >
           Gör en gratis förfrågan
         </button>
-        <div style={{ height: "500px" }}></div>
       </div>
     </section>
   );
